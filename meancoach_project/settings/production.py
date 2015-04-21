66e2bb29f02c8d2d2aa55793402934f87bb5f5d8
Eric Carmichael <eric@ckcollab.com>
2015-04-21 00:24:58 -0700

    turn off caching stuff

--- meancoach_project/settings/production.py
+++ meancoach_project/settings/production.py
@@ -12,9 +12,9 @@ ALLOWED_HOSTS = ['.herokuapp.com']
 
 # Use the cached template loader so template is compiled once and read from
 # memory instead of reading from disk on each load.
-TEMPLATES[0]['OPTIONS']['loaders'] = [
-    ('django.template.loaders.cached.Loader', [
-        'django.template.loaders.filesystem.Loader',
-        'django.template.loaders.app_directories.Loader',
-    ]),
-]
+# TEMPLATES[0]['OPTIONS']['loaders'] = [
+#     ('django.template.loaders.cached.Loader', [
+#         'django.template.loaders.filesystem.Loader',
+#         'django.template.loaders.app_directories.Loader',
+#     ]),
+# ]
