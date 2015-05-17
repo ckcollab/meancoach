65c1cc32c5a46548b2d6783f394a0732dc649343
Eric Carmichael <eric@ckcollab.com>
2015-05-16 23:30:52 -0700

    need to make it not stop on a single failed test..

--- meancoach_project/tests/functional/test_measurement_input.py
+++ meancoach_project/tests/functional/test_measurement_input.py
@@ -1,6 +1,6 @@
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
-from django.test.utils import override_settings
+# from django.test.utils import override_settings
 
 from .base import SeleniumTestCase
 
