6fd7123f2199a94f48064a3bc5e6bb0d7d37b7c1
Eric Carmichael <eric@ckcollab.com>
2015-11-11 03:55:18 -0800

    better default settings

--- meancoach_project/manage.py
+++ meancoach_project/manage.py
@@ -3,8 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    settings_module = os.environ.get('DJANGO_SETTINGS_MODULE', None)
-    assert settings_module, "DJANGO_SETTINGS_MODULE env variable not set"
+    settings_module = os.environ.get('DJANGO_SETTINGS_MODULE', 'settings.local')
 
     from django.core.management import execute_from_command_line
 
